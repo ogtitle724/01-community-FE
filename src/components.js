06f7ca3998bb12073c475705fca3c9f4e848a974
ogtitle724 <wje0724@gmail.com>
2023-05-05 16:49:48 +0900

    apply horizontal drag to trending list

--- src/components.js
+++ src/components.js
@@ -1,33 +1,69 @@
-export function Home() {
-  /* function handleMouseMove(e) {
-    e.target.addEventListener("mousemove", (event) =>
-      console.log(event.clientX)
-    );
-  } */
+import { useState } from "react";
 
+export function Home() {
   return (
     <div onScroll={() => console.log("scroll")} className="content--home">
-      <section
-        /* onMouseDown={(e) => handleMouseMove(e)} */
-        className="content__section-bests"
-      >
-        <h1 className="content__title--home">Trendings</h1>
-        {Array(10)
-          .fill("post")
-          .map((post, idx) => {
-            return <PostCard />;
-          })}
-      </section>
+      <HorizontalList />
       <section className="content__section-curr"></section>
     </div>
   );
 }
 
+function HorizontalList() {
+  let isMouseDown = false;
+  let startX;
+
+  function handleMouseDown(e) {
+    e.preventDefault();
+    isMouseDown = true;
+    startX = e.clientX;
+  }
+
+  function handleMouseUp(e) {
+    isMouseDown = false;
+  }
+
+  function handleDrag(e) {
+    if (!isMouseDown) return;
+
+    const degree = e.clientX - startX;
+    let container = document.getElementsByClassName(
+      "horizon-list__card-container"
+    )[0];
+
+    container.style.left = `${container.offsetLeft + degree}px`;
+    startX = e.clientX;
+  }
+
+  return (
+    <section className="horizon-list">
+      <h1 className="horizon-list__list-title">Trendings</h1>
+      <div
+        className="horizon-list__card-container"
+        onMouseDown={(e) => handleMouseDown(e)}
+        onMouseUp={(e) => handleMouseUp(e)}
+        onMouseLeave={(e) => handleMouseUp(e)}
+        onMouseMove={(e) => handleDrag(e)}
+      >
+        {Array(10)
+          .fill("post")
+          .map((post, idx) => {
+            return <PostCard key={"card_" + idx} />;
+          })}
+      </div>
+    </section>
+  );
+}
+
 function PostCard() {
   return (
-    <article className="card">
-      <img src="sample.png" className="card__img" alt="img dosen't supported" />
-      <h2 className="card__title">sample title </h2>
+    <article className="horizon-list__card">
+      <img
+        src="sample.png"
+        className="horizon-list__img"
+        alt="img dosen't supported"
+      />
+      <h2 className="horizon-list__card-title">sample title</h2>
     </article>
   );
 }
