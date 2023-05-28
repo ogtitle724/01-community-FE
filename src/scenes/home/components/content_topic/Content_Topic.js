2aa2956de5e7bd8d2d0dae7c1e22dbdb9ed2ab1e
ogtitle724 <wje0724@gmail.com>
2023-05-28 19:59:07 +0900

    adjust hit section in content topic scene

--- src/scenes/home/components/content_topic/Content_Topic.js
+++ src/scenes/home/components/content_topic/Content_Topic.js
@@ -11,25 +11,33 @@ export default function Others({ title, postData, page, setPage }) {
         page={page}
         setPage={setPage}
       />
-      <section className="hits">
-        <aside className="hits__wrapper">
-          <h2 className="hits__title">{"Hits"}</h2>
-          <Hits postNum={20} />
-          <Hits postNum={10} />
-        </aside>
+      <section className="main__hits">
+        <Hits />
       </section>
     </div>
   );
 }
-
-function Hits({ postNum }) {
+function Hits() {
   return (
-    <ul className="hits-board">
-      {Array(postNum)
+    <aside className="hits__wrapper">
+      <h2 className="hits__title">{"Hits"}</h2>
+      <HitTitles postNum={20} />
+      <HitComments postNum={10} />
+    </aside>
+  );
+}
+
+function HitTitles({ hitPostData }) {
+  return (
+    <ul className="hits-board hits-board--posts">
+      {Array(20)
         .fill("")
         .map((ele, idx) => {
           return (
-            <li key={idx + "hits"} className="hits-board__post">
+            <li
+              key={"hitTitle_" + idx}
+              className="hits-board__li hits-board__li--posts"
+            >
               <span
                 className={
                   "hits-board__order" +
@@ -39,8 +47,41 @@ function Hits({ postNum }) {
                 {idx + 1}
               </span>
 
-              <a href="/post?title=" className="hits-board__title">
-                {"여기에 따봉수로 컷한글 15개"}
+              <a href="/post?title=" className="hits-board__a">
+                <span className="hits-board__a-title center-y">
+                  {
+                    "최근 일정기간 조회수, 좋아요, 댓글 등을 바탕으로 정렬한 포스팅"
+                  }
+                </span>
+              </a>
+              <span className="hits-board__like">{12}</span>
+            </li>
+          );
+        })}
+    </ul>
+  );
+}
+
+function HitComments({ hitCommentData }) {
+  return (
+    <ul className="hits-board hits-board--comments">
+      {Array(10)
+        .fill("")
+        .map((ele, idx) => {
+          return (
+            <li key={idx + "hits"} className="hits-board__li">
+              <span
+                className={
+                  "hits-board__order" +
+                  (idx < 3 ? " hits-board__order--best" : "")
+                }
+              >
+                {idx + 1}
+              </span>
+              <a href="/post?title=" className="hits-board__a">
+                <span className="hits-board__a-title center-y">
+                  {"최근 일정기간 좋아요 증가순 정렬한 댓글"}
+                </span>
               </a>
               <span className="hits-board__like">{12}</span>
             </li>
